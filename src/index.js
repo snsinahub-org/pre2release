e0a130c23d4e25f7567d75dcea60b192e4d9f32f
snsinahub <snsinahub@github.com>
2023-04-19 16:59:02 -0400

    add logger

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ async function run() {
     } else if(REQUIRE_PRERELEASE == 'false' && prereleaseIsNewest == false) {
         latestVersion = tagsObj[0].name //jsonUtils.firstItem('tagName');
     } else {
-        core.setFailed('Error: No release found');
+        // core.setFailed('Error: No release found');
     }
 
     console.log(prereleaseIsNewest, latestVersion, REQUIRE_PRERELEASE)
