7eda4bef6102109479437ffc382cf59a43c382b1
snsinahub <snsinahub@github.com>
2023-04-19 15:22:58 -0400

    fix the bug

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ async function run() {
 
 
     } else if(REQUIRE_PRERELEASE == 'false' && prereleaseIsNewest == false) {
-        console.log(JSON.stringify(jsonUtils.jsonObj[0]))
+        console.log(JSON.stringify(tagsObj))
         latestVersion = jsonUtils.jsonObj[0].tag //jsonUtils.firstItem('tagName');
     } else {
         // latestVersion = "Error: No release found";
